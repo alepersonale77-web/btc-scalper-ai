255614860f418ca21990e271e523bb9e8a5d6624
alepersonale77-web <alepersonale77@gmail.com>
2026-08-04 00:28:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import aiohttp
 from datetime import datetime
 
 
-SYMBOL = "BTCUSDT"
+SYMBOL = "BTCUSD"
 INTERVAL = "15m"
 
 
@@ -26,44 +26,36 @@ async def get_btc_price():
     close_price = float(last_candle[4])
 
     return open_price, close_price
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            data = await response.json()
-
-    if not isinstance(data, list):
-        raise Exception(f"Risposta Binance non valida: {data}")
-
-    last_candle = data[-1]
-
-    open_price = float(last_candle[1])
-    close_price = float(last_candle[4])
-
-    return open_price, close_price
 
 
 async def main():
-  print("🚀 BTC Trend AI avviato", flush=True) 
 
-while True:
-    try:
+    print("🚀 BTC Trend AI avviato", flush=True)
+
+    while True:
+        try:
             open_price, close_price = await get_btc_price()
 
             now = datetime.now().strftime("%H:%M:%S")
 
-            direction = "RIALZO" if close_price > open_price else "RIBASSO"
+            direction = (
+                "RIALZO"
+                if close_price > open_price
+                else "RIBASSO"
+            )
 
             print(
-                f"{now} | BTCUSDT M15 | "
+                f"{now} | BTCUSD M15 | "
                 f"Open: {open_price} | "
                 f"Close: {close_price} | "
                 f"Movimento: {direction}",
                 flush=True
             )
 
-     except Exception as e:
-            print("Errore Binance:", repr(e), flush=True)
+        except Exception as e:
+            print("Errore Coinbase:", repr(e), flush=True)
 
-     await asyncio.sleep(60)
+        await asyncio.sleep(60)
 
 
 if __name__ == "__main__":
